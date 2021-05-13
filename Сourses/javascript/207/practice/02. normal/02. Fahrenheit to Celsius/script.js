66910775a8f3bd6c399c736501c0b69b6a9ba142
Ilya ZoolooS Popov <zooloosik@gmail.com>
2021-05-13 22:05:05 +0300

    add Simple JS. Medium Challenges

---
+++ Сourses/javascript/207/practice/02. normal/02. Fahrenheit to Celsius/script.js
@@ -0,0 +1,16 @@
+let fahrenheitTemperature = 77;
+
+let celsiusTemperature = 5 / 9 * (fahrenheitTemperature - 32);
+console.log(celsiusTemperature);
+
+/* Техническое задание
+
+Мяу! Напиши программу, которая будет переводить градусы Фаренгейта в температуру по Цельсию.
+
+В переменной fahrenheitTemperature указаны градусы по Фаренгейту.
+
+Формула расчёта выглядит так: Tc = 5/9 * (Tf - 32).
+
+Результат вычислений запиши в переменную celsiusTemperature.
+
+*/
