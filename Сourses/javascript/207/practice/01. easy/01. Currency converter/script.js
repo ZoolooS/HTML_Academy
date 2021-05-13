f6ca0b56ef3bd6496cc6742abe26cf32420489c7
Ilya ZoolooS Popov <zooloosik@gmail.com>
2021-05-13 21:46:06 +0300

    add JS Simple Challenges

---
+++ Сourses/javascript/207/practice/01. easy/01. Currency converter/script.js
@@ -0,0 +1,21 @@
+let euroRate = 74;
+let dollarRate = 63;
+
+let euroAmount = 500;
+let dollarAmount = 2500;
+
+let rublesAmount = euroAmount * euroRate + dollarAmount * dollarRate;
+
+console.log(rublesAmount);
+
+/* Техническое задание
+
+Напиши программу, которая посчитает сколько всего рублей понадобится на поездку.
+
+Курсы валют указаны в переменных euroRate и dollarRate.
+
+Переменные euroAmount и dollarAmount— необходимые суммы на поездку.
+
+Создай переменную rublesAmount и записывай в неё результат вычислений.
+
+*/
