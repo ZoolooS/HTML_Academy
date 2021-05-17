a50b520580c84dba1558947fe92acf105c2ab3cc
Ilya ZoolooS Popov <zooloosik@gmail.com>
2021-05-17 22:27:53 +0300

    add JS Loops. Easy Challenges

---
+++ Сourses/javascript/211/practice/01. easy/03. Product of even/script.js
@@ -0,0 +1,16 @@
+let lastNumber = 5;
+let multiplicationResult = 1;
+
+for (let pointer = 2; pointer <= lastNumber; pointer += 2) {
+  multiplicationResult *= pointer;
+};
+
+/* Техническое задание
+
+Напишите универсальную программу, которая находит произведение всех чётных чисел из последовательности от 1 до n.
+
+Число, до которого идёт последовательность (включительно), записано в переменную lastNumber
+
+Найдите произведение всех чисел и сохраните результат в переменную multiplicationResult.
+
+*/
