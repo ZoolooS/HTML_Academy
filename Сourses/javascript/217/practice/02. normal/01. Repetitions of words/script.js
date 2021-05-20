ee65ae7ef48dc3296240d438ebabbcb3bda42115
Ilya ZoolooS Popov <zooloosik@gmail.com>
2021-05-20 18:02:43 +0300

    add JS Objects. Easy Challenges

---
+++ Сourses/javascript/217/practice/02. normal/01. Repetitions of words/script.js
@@ -0,0 +1,43 @@
+let getRepeats = function (rawData) {
+  let outputData = {};
+  let outputWords = [rawData[0]];
+  let outputNumbers = [1];
+  
+  for (let i = 1; i < rawData.length; ++i) {
+    let counter = 0;
+    for (let j = 0; j < outputWords.length; ++j) {
+      if (rawData[i] === outputWords[j]) {
+        ++outputNumbers[j];
+        break;
+      }
+      ++counter;
+      
+      if (counter === outputWords.length) {
+        outputWords.push(rawData[i]);
+        outputNumbers.push(1);
+        break;
+      }
+    }
+  }
+  
+  // console.log('outputWords: ' + outputWords.length + '\n' + outputWords);
+  // console.log('outputNumbers: ' + outputNumbers.length + '\n' + outputNumbers);
+  
+  for (let i = 0; i < outputWords.length; ++i) {
+    outputData[outputWords[i]] = outputNumbers[i];
+  }
+  
+  return outputData;
+};
+
+
+/*
+
+В этой задаче вам нужно проанализировать данные — вычислить повторы каждого слова.
+
+Создайте функцию getRepeats с одним параметром. В этот параметр будет приходить массив данных.
+
+Функция должна возвращать объект, в котором указано сколько раз каждое слово встречается в массиве.
+
+
+*/
